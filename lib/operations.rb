81b1778b256ff423b101130ed8f20f381e735689
eaylward8 <eaylward8@gmail.com>
2015-11-06 13:36:49 -0500

    Fixed operations.rb

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,14 @@
 def unsafe?(speed)
-
+  if speed > 60 || speed < 40
+    true
+  else
+    false
+  end
 end
 
-
-
 def not_safe?(speed)
-	
+  #between? is inclusive
+  speed.between?(40, 60) ? false : true	
 end
 	
 
