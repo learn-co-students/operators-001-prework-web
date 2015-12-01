b15c6db55b103eb0912bb041a9ee2dc3754bc05e
flats <dave@robador.com>
2015-12-01 11:25:43 -0500

    alternative solutions

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,15 +1,24 @@
 def unsafe?(speed)
-  if speed < 40 || speed > 60
-    return true
-  else
-    return false
-  end
+  # if speed < 40 || speed > 60
+  #   return true
+  # else
+  #   return false
+  # end
+  # OR
+  # !((40..60).include?(speed))
+  # OR
+  # !safe?(speed)
+  speed > 60 || speed < 40
 end
 
+def safe?(speed)
+  (40..60).include?(speed)
+end
 
 
 def not_safe?(speed)
 	speed < 40 || speed > 60 ? true : false
+  # unsafe(speed) ? true : false
 end
 	
 
