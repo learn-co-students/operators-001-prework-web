9a7b639b2057abbc50d165e2bad24f0c324c5cb9
rramdehol <rramdeholshift@gmail.com>
2015-12-09 17:22:17 -0500

    Added operations.rb with ternary

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,20 @@
 def unsafe?(speed)
-
+  if (speed > 60)
+    response = true 
+    response 
+  elsif (speed < 40)
+    response = true 
+    response 
+  else 
+    response = false
+    response 
+  end 
 end
 
 
 
 def not_safe?(speed)
-	
+	(speed > 60 || speed < 40) ? true : false 
 end
 	
 
