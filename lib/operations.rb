4103ce94db58a1b4e879e699e69bf471bb8b608e
Simara23 <loere.me@gmail.com>
2015-12-10 15:35:46 +0100

    operator ?

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,14 @@
 def unsafe?(speed)
-
+  if speed < 45 || speed > 60
+    result = true
+  else 
+  result = false
 end
-
-
+  return result
+end
 
 def not_safe?(speed)
-	
+  speed < 45 || speed > 60 ? true : false   
 end
-	
 
-
+print unsafe?(72)
