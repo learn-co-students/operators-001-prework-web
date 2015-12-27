33de5f64aa00d2b248f7acc321074318f2466fdf
UrickBaptiste <urick_baptiste@hotmail.com>
2015-12-27 02:51:01 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,20 @@
 def unsafe?(speed)
+  speed == 25
 
+  if (speed < 40 || speed > 60)
+    return true
+  else
+    return false
+end
 end
 
 
 
 def not_safe?(speed)
-	
+  speed == 25
+  speed < 40 || speed > 60 ? true : false
 end
-	
+
+
 
 
