f9e262d1df12ae37097efde8f4f9053d235e3c9e
CodeByLine <yleventhal@ymail.com>
2016-01-18 16:33:43 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -2,15 +2,21 @@ require 'pry'
 
 def unsafe?(speed)
 
-  speed <40 && speed > 60
-   
+  if speed <= 40
+    return true
+  elsif speed >= 60
+    return true
+  else
+    return false
+  end
 end
-
 
 
 def not_safe?(speed)
-	
+  speed <= 40 or speed >= 60? true : false
+  #speed > 60? true : false
+
 end
-	
+#binding.pry
 
 
