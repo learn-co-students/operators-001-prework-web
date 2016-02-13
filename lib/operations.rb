d46c31f36ab0201ae444973ee5a8953d770c1e3f
achesky <afc340@nyu.edu>
2016-02-13 19:23:05 +0700

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,11 @@
-def unsafe?(speed)
-
+def unsafe?(speed = int)
+  (speed < 40 or speed > 60) ? true : false
 end
 
 
 
 def not_safe?(speed)
-	
+	(speed > 60 or speed < 40) ? true : false
 end
 	
 
