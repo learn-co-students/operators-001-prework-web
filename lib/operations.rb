7cd95eaef5fadb3eba5bd3ee83a9af844cf54f01
ericastevens <estevens@nyc.yearup.org>
2016-02-16 02:58:13 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,16 @@
 def unsafe?(speed)
-
+  if (speed < 40) || (speed > 60)
+    true
+  else
+    false
+  end
 end
 
-
+unsafe?(30)
 
 def not_safe?(speed)
-	
+  (speed < 40) || (speed > 60) ? true : false
 end
-	
+
 
 
