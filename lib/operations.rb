536de45616e5c7868f00b4433da18dc372bc16cb
Mario245 <mario.peart@live.com>
2016-04-06 20:22:50 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,23 @@
+require 'pry'
 def unsafe?(speed)
-
+  if speed > 60
+    return true
+  elsif speed < 40
+    return true
+  elsif speed > 40 && speed < 60
+    return false
+  end
 end
-
-
 
 def not_safe?(speed)
-	
+  ( speed > 60 ? true : (speed < 40 ? true : (speed < 60 && speed > 40 ? false : true)))
+
 end
-	
 
+value = not_safe?(65)
+puts value
+value = not_safe?(35)
+puts value
 
+value = not_safe?(45)
+puts value
