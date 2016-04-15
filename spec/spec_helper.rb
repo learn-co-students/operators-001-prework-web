ca49d72d85b7450b68f800cedd74c6f7fa4bd9fb
stephNA <310srh@gmail.com>
2016-04-14 17:11:53 -0700

    Resolved a few issues with the operations_spec.rb file that made this lab very difficult to pass

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,5 +2,9 @@
 require_relative '../lib/operations.rb'
 
 RSpec.configure do |config|
+    #config.treat_symbols_as_meta_data_keys_with_true_values = true
+    #config.run_all_when_everything_filtered = true
+    #config.filter_run :focus
+    config.order = 'default'
   # config here
 end
