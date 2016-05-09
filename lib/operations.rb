fcd38120b8cb59f18a3828b46458894c113af527
Ece Ozalp <ozale272@newschool.edu>
2016-05-09 10:16:38 -0400

    :> exit

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,12 @@
 def unsafe?(speed)
-
+	if (speed > 60)
+		return true
+ elsif (speed < 40)
+	 return true
+ else false
 end
-
-
+end
 
 def not_safe?(speed)
-	
+	(60 < speed) ? true : (40 > speed) ? true : false
 end
-	
-
-
