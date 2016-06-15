0218f96b712f4878316c86275dc859bbb5cf0fa7
Hafizah Geter <haf.geter@gmail.com>
2016-06-15 02:57:47 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,15 @@
 def unsafe?(speed)
 
-end
+  if (speed < 40) || (speed > 60)
+		return true
+	else
+		return false
+  end
+ end
 
 
 
-def not_safe?(speed)
-	
-end
-	
+ def not_safe?(speed)
 
-
+  (speed > 60) || (speed < 40) ? true : false
+ end
