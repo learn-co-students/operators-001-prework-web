d03746e0e540bd3bc2baf3c32b3f5c1c378833fd
Dallin Wardrop <dallinwardrop12@gmail.com>
2016-06-16 20:47:04 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,26 @@
 def unsafe?(speed)
-
+	if speed > 60
+		return true
+	end
+	if speed < 40
+		return true
+	end
+	if speed < 60
+		return false
+	end
+	if speed > 40
+		return false
+	end
 end
 
 
 
 def not_safe?(speed)
 	
+speed > 60 ? true : false
+
+speed < 40 ? true : false
+
+speed = (40-60) ? false : true
+
 end
-	
-
-
