5142df2564ca3aff7e1ba3f955f05deabe2ff45d
hilton mak <hiltonmak@yahoo.om>
2016-06-26 12:39:46 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,38 @@
 def unsafe?(speed)
 
+  if speed > 60
+		#answer ="returns true if speed is greater than 60"
+		answer = true
+		return answer
+  elsif speed >= 40
+		#answer = "returns false if the speed is between 40 and 60"
+		answer = false
+		return answer
+	else
+		#answer = "returns true if speed is less than 40"
+		answer = true
+		return answer
+  end
+
 end
 
 
 
 def not_safe?(speed)
-	
+	if speed > 60
+		#answer ="returns true if speed is greater than 60"
+		answer = true
+		return answer
+  elsif speed >= 40
+		#answer = "returns false if the speed is between 40 and 60"
+		answer = false
+		return answer
+	elsif
+		#answer = "returns true if speed is less than 40"
+		answer = true
+		return answer
+  else
+    return "?"
+  end
+
 end
-	
-
-
