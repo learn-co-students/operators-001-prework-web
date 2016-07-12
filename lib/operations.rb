e7eb55a3ed1d1035b2992ba87fb7e78ef36fc9e5
Jess Harrelson <jess.harrelson0707@gmail.com>
2016-07-11 22:32:10 -0400

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,31 @@
 def unsafe?(speed)
-
+  if speed > 60
+    true
+  elsif speed < 40
+    true
+  else
+    false
+  end
 end
 
 
 
 def not_safe?(speed)
-	
+unsafe?(speed) != false ? true : false
 end
-	
 
-
+#def too_fast
+#  speed > 60
+#  true
+#end
+#
+#def too_slow
+#  speed < 40 ? true : false
+#  true
+#end
+#
+#
+#def bad
+#  false
+#end
+#
