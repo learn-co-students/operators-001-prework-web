18a34964e1b855b29cd25c99a37b029c916e6d71
Michael Wiggins <michael.wiggins@me.com>
2016-07-26 14:01:08 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,22 @@
-def unsafe?(speed)
-
-end
-
-
-
 def not_safe?(speed)
-	
+	speed > 60 ? true : false
+	speed < 40 ? true : false
+	speed > 40 && speed < 60 ? false : true
+
+	end
+
+	def unsafe?(speed)
+		if speed > 60
+		true
+
+	elsif speed < 40
+		true
+
+	elsif speed > 40 && speed < 60
+		false
+
+	else
+		nil
+
+	end
 end
-	
-
-
