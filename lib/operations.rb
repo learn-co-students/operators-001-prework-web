a156495c2294b24510528390ecb3e03657693341
Michael Wiggins <michael.wiggins@me.com>
2016-08-13 17:09:13 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,3 +1,9 @@
+#conditional ? action_if_true : action_if_false
+#def crybaby(age)
+#age = 1
+#age < 3 ? "CryBaby" : "Not a crybaby."
+#end
+
 def not_safe?(speed)
 	speed > 60 ? true : false
 	speed < 40 ? true : false
